0ef62f85f70664c2a9a9155635c2cc13625a2f0d
Steven <fukasesteven@gmail.com>
2022-06-19 16:30:10 +0900

    iterators 4

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,47 @@
-pub fn factorial(num: u64) -> u64 {
-    // Complete this function to return the factorial of num
-    // Do not use:
-    // - return
-    // Try not to use:
-    // - imperative style loops (for, while)
-    // - additional variables
-    // For an extra challenge, don't use:
-    // - recursion
-    // Execute `rustlings hint iterators4` for hints.
-    // 6! = 6 * 5 * 4 * 3 * 2 * 1;
-    let nums = [0..num];
-    let nums_iter = nums.iter().fold(0, |acc, x| acc * x);
-    println!("{:?}", nums_iter);
-    todo!();
+
+use std::collections::HashMap;
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum Progress {
+    None,
+    Some,
+    Complete,
 }
 
+fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
+    let mut count = 0;
+    for val in map.values() {
+        if val == &value {
+            count += 1;
+        }
+    }
+    count
+}
+
+fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
+    // map is a hashmap with String keys and Progress values.
+    // map = { "variables1": Complete, "from_str": None, ... }
+}
+
+fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
+    let mut count = 0;
+    for map in collection {
+        for val in map.values() {
+            if val == &value {
+                count += 1;
+            }
+        }
+    }
+    count
+}
+
+fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
+    // collection is a slice of hashmaps.
+    // collection = [{ "variables1": Complete, "from_str": None, ... },
+    //     { "variables2": Complete, ... }, ... ]
+}
+
+
 fn main() {
-    println!("{:?}", factorial(6));
-    println!("{:?}", factorial(0));
+
 }
