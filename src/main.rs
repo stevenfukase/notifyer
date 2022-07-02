67f9ae7f22ede2013c1c5472d9bc79bd48113696
Steven <fukasesteven@gmail.com>
2022-07-02 14:31:47 +0900

    rust init

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,1 @@
-use std::env;
-
-fn main() {
-    let args = env::args();
-    if args.len() != 3 {
-        panic!("invalid arg count")
-    }
-    println!("{:?}", args);
-}
+fn main() {}
---
+++ target/debug/deps/playground-c3f4d113e616aa05.d
@@ -0,0 +1,5 @@
+/Users/fantaisie-impromptu/Documents/Projects/playground/target/debug/deps/playground-c3f4d113e616aa05.rmeta: src/main.rs
+
+/Users/fantaisie-impromptu/Documents/Projects/playground/target/debug/deps/playground-c3f4d113e616aa05.d: src/main.rs
+
+src/main.rs:
