1ca11db103e58063dca8272ffeca76c35589663f
Steven <fukasesteven@gmail.com>
2022-07-03 15:15:13 +0900

    deps fix

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,12 @@
-fn main() {}
+use rust_gpiozero::*;
+
+fn main() {
+    // Create a new LED attached to Pin 17
+    let mut led = LED::new(17);
+
+    // on_time = 2 secs, off_time=3 secs
+    led.blink(2.0, 3.0);
+
+    // prevent program from exiting immediately
+    led.wait();
+}
