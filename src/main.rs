31a7ba994fd5d9535eb8fed022fa7ec92d532080
Steven <fukasesteven@gmail.com>
2022-07-03 22:24:03 +0900

    fidget

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-#![warn(clippy::all, rust_2018_idioms)]
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+// #![warn(clippy::all, rust_2018_idioms)]
+// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
@@ -9,7 +9,7 @@ fn main() {
 
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
-        "eframe template",
+        "🦀Dashboard",
         native_options,
         Box::new(|cc| Box::new(playground::TemplateApp::new(cc))),
     );
