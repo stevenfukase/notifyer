8c65ba8823f6c795a78c6750b5f7f8ae2386b001
Steven <fukasesteven@gmail.com>
2022-07-04 23:44:46 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use log::{error, warn};
+
 pub struct App {
     pub blocks: Vec<Block>,
 }
@@ -34,12 +36,40 @@ impl App {
         if self.is_block_valid(last_block, block) {
             self.blocks.push(block);
         } else {
-            eprintln!("Block invalid");
+            error!("Invalid");
         }
     }
 
-    fn is_block_valid(&self, last_block: Block, new_block: Block) {
-        
+    fn is_block_valid(&self, last_block: Block, new_block: Block) -> bool {
+        if last_block.hash != new_block.prev_hash {
+            warn!("Wrong prev_hash");
+            false
+        }
+        if !hash_to_binary_representation(&hex::decode(&block.hash).expect("Can decode from hex"))
+            .starts_with(DIFFICULTY_PREFIX)
+        {
+            warn!("block with id: {} has invalid difficulty", block.id);
+            return false;
+        }
+        if block.id != previous_block.id + 1 {
+            warn!(
+                "block with id: {} is not the next block after the latest: {}",
+                block.id, previous_block.id
+            );
+            return false;
+        }
+        if hex::encode(calculate_hash(
+            block.id,
+            block.timestamp,
+            &block.previous_hash,
+            &block.data,
+            block.nonce,
+        )) != block.hash
+        {
+            warn!("block with id: {} has invalid hash", block.id);
+            return false;
+        }
+        true
     }
 }
 
