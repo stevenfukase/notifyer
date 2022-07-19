d614a0cc68543617055ac3f0526b0fe27702b1db
Steven Fukase <fukasesteven@gmail.com>
2022-07-19 18:20:13 +0900

    notifyer

---
+++ src/constants.rs
@@ -0,0 +1,1 @@
+pub const SLACK_SEND_MESSAGE_ENDPOINT: &str = "https://slack.com/api/chat.postMessage";
--- src/slack_notify.rs
+++ src/slack_notify.rs
@@ -2,19 +2,16 @@ use std::collections::HashMap;
 
 pub mod constants;
 
-#[tokio::main]
-async fn main() {
-    let message = "Hello from rust!";
+pub async fn slack_notify(message: &str) {
     let request_body = HashMap::from([
         ("channel", env!("SLACK_CHANNEL_ID")),
         ("text", message),
     ]);
     let client = reqwest::Client::new();
-    let response = client
+    client
         .post(constants::SLACK_SEND_MESSAGE_ENDPOINT)
         .bearer_auth(env!("SLACK_BOT_USER_OAUTH_TOKEN"))
         .json(&request_body)
         .send()
-        .await;
-    println!("{:?}", response);
+        .await
 }
