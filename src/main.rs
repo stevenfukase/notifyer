2304c20ba9452d60f48cfea76775a29556f3edac
Steven Fukase <fukasesteven@gmail.com>
2022-07-19 18:38:08 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::collections::HashMap;
-
+pub mod constants;
+pub mod slack_notify;
 
 #[tokio::main]
 async fn main() {
     let message = "Hello from rust!";
+    slack_notify::slack_notify(message);
 }
--- src/slack_notify.rs
+++ src/slack_notify.rs
@@ -1,12 +1,9 @@
 use std::collections::HashMap;
 
-pub mod constants;
+use crate::constants;
 
 pub async fn slack_notify(message: &str) {
-    let request_body = HashMap::from([
-        ("channel", env!("SLACK_CHANNEL_ID")),
-        ("text", message),
-    ]);
+    let request_body = HashMap::from([("channel", env!("SLACK_CHANNEL_ID")), ("text", message)]);
     let client = reqwest::Client::new();
     client
         .post(constants::SLACK_SEND_MESSAGE_ENDPOINT)
