771ad3222eeee0f941b2b5b6143f41ff699ebba3
Steven <fukasesteven@gmail.com>
2022-07-21 01:13:14 +0900

    wip github

--- src/lib/github.rs
+++ src/lib/github.rs
@@ -1,3 +1,16 @@
-pub fn get_today_activity() {
-  
-}
+const GITHUB_ENDPOINT: &str = "https://api.github.com/graphql";
+
+use graphql_client::GraphQLQuery;
+
+type Date = String;
+type DateTime = String;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "src/lib/github_schema.graphql",
+    query_path = "src/lib/user_contributions.graphql",
+    response_derives = "Serialize,PartialEq"
+)]
+pub struct UserContributions;
+
+pub fn get_today_activity() {}
