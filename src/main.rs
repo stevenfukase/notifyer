c62b0fcc1e9a2fb6e95683c6e2af5eea3c9d64ef
Steven <fukasesteven@gmail.com>
2022-07-23 11:46:52 +0900

    send json

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,12 @@ mod lib;
 
 #[tokio::main]
 async fn main() {
-    let message = "Hello from rustberry!";
     let delay = time::Duration::from_secs(3);
     let github_stat = github::get_activity().await;
-    println!("{:?}", github_stat);
 
+    let message = format!("{:?}", github_stat);
     loop {
-        let result = slack::notify(message).await;
+        let result = slack::notify(&message).await;
 
         if result.is_ok() {
             println!("Executed");
