da284bad706c37f4869abcff8a02ab893a4369d6
Steven <fukasesteven@gmail.com>
2022-07-24 01:50:30 +0900

    extract ended_at

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,28 @@ mod lib;
 #[tokio::main]
 async fn main() {
     let delay = time::Duration::from_secs(3);
-    let github_stat = github::get_activity().await;
+    let ended_at = github::get_activity()
+        .await
+        .unwrap()
+        .data
+        .unwrap()
+        .user
+        .unwrap()
+        .contributions_collection
+        .ended_at;
 
-    let message = format!("{:?}", github_stat);
-    loop {
-        let result = slack::notify(&message).await;
+    let message = format!("{:?}", ended_at);
+    println!("{:?}", message);
 
-        if result.is_ok() {
-            println!("Executed");
-            break;
-        }
-        println!("Failed to send Slack");
+    // loop {
+    //     let result = slack::notify(&message).await;
 
-        thread::sleep(delay);
-    }
+    //     if result.is_ok() {
+    //         println!("Executed");
+    //         break;
+    //     }
+    //     println!("Failed to send Slack");
+
+    //     thread::sleep(delay);
+    // }
 }
