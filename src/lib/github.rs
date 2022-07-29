5dfa97fdff42436a854512ea2ead0ceb46892adc
Steven <fukasesteven@gmail.com>
2022-07-29 20:04:17 +0900

    fixed error

--- src/lib/github.rs
+++ src/lib/github.rs
@@ -1,4 +1,5 @@
-use graphql_client::GraphQLQuery;
+use chrono::{SecondsFormat, Utc};
+use graphql_client::{GraphQLQuery, Response};
 use reqwest::Client;
 
 type DateTime = String;
@@ -10,11 +11,11 @@ type Date = String;
     query_path = "src/lib/graphql/user_contributions.graphql",
     response_derives = "Debug,Serialize,PartialEq"
 )]
-pub struct UserContributions;
+pub struct SingleDayContributions;
 
 const GITHUB_ENDPOINT: &str = "https://api.github.com/graphql";
 
-pub async fn todays_activity_count() -> Result<reqwest::Response, reqwest::Error> {
+pub async fn todays_contribution_count() -> Result<i64, reqwest::Error> {
     let github_username = env!("GITHUB_USERNAME");
     let github_token = env!("GITHUB_ACCESS_TOKEN");
 
@@ -30,14 +31,35 @@ pub async fn todays_activity_count() -> Result<reqwest::Response, reqwest::Error
         )
         .build()?;
 
-    let login = github_username.to_string();
-    let variables = user_contributions::Variables { login };
+    let variables = single_day_contributions::Variables {
+        login: github_username.to_string(),
+        date: Utc::now().to_rfc3339_opts(SecondsFormat::Secs, true),
+    };
+
+    let request_body = SingleDayContributions::build_query(variables);
 
     // graphql_client::reqwest::post_graphql will cause error
     // when compiling for armv7-unknown-linux-gnueabihf
-    client
+    let response = client
         .post(GITHUB_ENDPOINT)
-        .json(&UserContributions::build_query(variables))
+        .json(&request_body)
         .send()
-        .await
+        .await?;
+
+    let parsed_response = response
+        .json::<Response<single_day_contributions::ResponseData>>()
+        .await?;
+
+    let contribution_count = parsed_response
+        .data
+        .unwrap()
+        .user
+        .unwrap()
+        .contributions_collection
+        .contribution_calendar
+        .weeks[0]
+        .contribution_days[0]
+        .contribution_count;
+
+    Ok(contribution_count)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,35 @@
-use lib::{github::{self, UserContributions}, slack};
+use lib::{github, slack};
 use std::{thread, time};
 mod lib;
 
 #[tokio::main]
 async fn main() {
     let delay = time::Duration::from_secs(3);
-    let result = github::todays_activity_count().await.unwrap().json::<user_contributions::Response>().await;
-    println!("{:?}", result);
-    
+    let contribution_count = &github::todays_contribution_count().await.unwrap_or(0);
 
-    // if activity_count == &0 {
-    //     let message = "You haven't committed today.";
-    //     loop {
-    //         let result = slack::notify(message).await;
-    //         if result.is_ok() {
-    //             println!("Executed");
-    //             break;
-    //         }
-    //         println!("Failed to send Slack");
+    if contribution_count == &0 {
+        let message = "You haven't committed today.";
+        loop {
+            let result = slack::notify(message).await;
+            if result.is_ok() {
+                println!("Executed");
+                break;
+            }
+            println!("Failed to send Slack");
 
-    //         thread::sleep(delay);
-    //     }
-    // } else {
-    //     let message = "You have committed at least once today.";
-    //     loop {
-    //         let result = slack::notify(message).await;
-    //         if result.is_ok() {
-    //             println!("Executed");
-    //             break;
-    //         }
-    //         println!("Failed to send Slack");
+            thread::sleep(delay);
+        }
+    } else {
+        let message = "You have committed at least once today.";
+        loop {
+            let result = slack::notify(message).await;
+            if result.is_ok() {
+                println!("Executed");
+                break;
+            }
+            println!("Failed to send Slack");
 
-    //         thread::sleep(delay);
-    //     }
-    // }
+            thread::sleep(delay);
+        }
+    }
 }
