abdeadadc3ee4f6aa5837f67f2b3068a193140f9
Steven <fukasesteven@gmail.com>
2022-07-29 20:30:46 +0900

    simplified

--- src/lib/slack.rs
+++ src/lib/slack.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::{thread, time};
 
 const SLACK_SEND_MESSAGE_ENDPOINT: &str = "https://slack.com/api/chat.postMessage";
 
@@ -11,4 +12,17 @@ pub async fn notify(message: &str) -> Result<reqwest::Response, reqwest::Error> 
         .json(&request_body)
         .send()
         .await
+}
+
+pub async fn send_with_retry(message: &str) {
+    let delay = time::Duration::from_secs(3);
+    loop {
+        let result = notify(message).await;
+        if result.is_ok() {
+            println!("Executed");
+            break;
+        }
+        println!("Failed to send Slack. Retrying");
+        thread::sleep(delay);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,14 @@
-use lib::{github, slack};
-use std::{thread, time};
+use lib::{github, slack::send_with_retry};
 mod lib;
 
 #[tokio::main]
 async fn main() {
-    let delay = time::Duration::from_secs(3);
     let contribution_count = &github::todays_contribution_count().await.unwrap_or(0);
 
-    if contribution_count == &0 {
-        let message = "You haven't committed today.";
-        loop {
-            let result = slack::notify(message).await;
-            if result.is_ok() {
-                println!("Executed");
-                break;
-            }
-            println!("Failed to send Slack");
-
-            thread::sleep(delay);
-        }
+    let message = if contribution_count == &0 {
+        "You haven't committed today."
     } else {
-        let message = "You have committed at least once today.";
-        loop {
-            let result = slack::notify(message).await;
-            if result.is_ok() {
-                println!("Executed");
-                break;
-            }
-            println!("Failed to send Slack");
-
-            thread::sleep(delay);
-        }
-    }
+        "You have committed at least once today."
+    };
+    send_with_retry(message).await;
 }
