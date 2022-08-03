878d315cbd20a804e895893d6ea695c5987b697c
Steven <fukasesteven@gmail.com>
2022-08-03 12:14:06 +0900

    Impl block message

--- src/lib/slack/mod.rs
+++ src/lib/slack/mod.rs
@@ -1,12 +1,15 @@
-use std::collections::HashMap;
+use serde_json::{json, Value};
 use std::{thread, time};
 
 const SLACK_SEND_MESSAGE_ENDPOINT: &str = "https://slack.com/api/chat.postMessage";
 
-pub async fn notify(message: &str) -> Result<reqwest::Response, reqwest::Error> {
-    let request_body = HashMap::from([("channel", env!("SLACK_CHANNEL_ID")), ("text", message)]);
-    let client = reqwest::Client::new();
-    client
+pub async fn perform_request(message_blocks: Value) -> Result<reqwest::Response, reqwest::Error> {
+    let request_body = json!({
+        "channel": env!("SLACK_CHANNEL_ID"),
+        "blocks": message_blocks["blocks"],
+    });
+
+    reqwest::Client::new()
         .post(SLACK_SEND_MESSAGE_ENDPOINT)
         .bearer_auth(env!("SLACK_BOT_USER_OAUTH_TOKEN"))
         .json(&request_body)
@@ -14,10 +17,10 @@ pub async fn notify(message: &str) -> Result<reqwest::Response, reqwest::Error> 
         .await
 }
 
-pub async fn send_with_retry(message: &str) {
+pub async fn send(blocks: Value) {
     let delay = time::Duration::from_secs(3);
     loop {
-        let result = notify(message).await;
+        let result = perform_request(blocks.clone()).await;
         if result.is_ok() {
             println!("Executed");
             break;
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use lib::{github, slack::send_with_retry};
+use lib::{github, slack};
+use serde_json::json;
 mod lib;
 
 #[tokio::main]
@@ -6,9 +7,41 @@ async fn main() {
     let contribution_count = &github::todays_contribution_count().await.unwrap_or(0);
 
     let message = if contribution_count == &0 {
-        "You haven't committed today."
+        json!({
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "You haven't committed today."
+                    }
+                }
+            ]
+        })
     } else {
-        "You have committed at least once today."
+        // has_contribution_message()
+        json!({
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                      "type": "mrkdwn",
+                      "text": you_have_commited_msg(contribution_count)
+                    }
+                }
+            ]
+        })
     };
-    send_with_retry(message).await;
+    slack::send(message).await;
+}
+
+fn you_have_commited_msg(contribution_count: &i64) -> String {
+    let count_string = if contribution_count == &1 {
+        "once".to_owned()
+    } else if contribution_count == &2 {
+        "twice".to_owned()
+    } else {
+        format!("{} times", contribution_count)
+    };
+    format!("You have commited {} today.", count_string)
 }
