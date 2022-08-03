2b134f048b4c6aa43bb2fad6c78387a80f47d611
Steven <fukasesteven@gmail.com>
2022-08-03 23:27:37 +0900

    cleanup

--- src/repositories/message_repository_abstract.rs
+++
@@ -1,7 +0,0 @@
-use async_trait::async_trait;
-use std::error::Error;
-
-#[async_trait(?Send)]
-pub trait MessageRepositoryAbstract {
-    async fn send(&self, message: String) -> Result<(), Box<dyn Error>>;
-}
--- src/usecases/interfaces.rs
+++
@@ -1,5 +0,0 @@
-
-#[async_trait(?Send)]
-pub trait AbstractUsecase<T> {
-    async fn execute(&self) -> Result<T, Error>;
-}
