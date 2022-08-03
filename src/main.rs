ce1d340d02ccea727d8b20e2e3aec64d8a726444
Steven <fukasesteven@gmail.com>
2022-08-03 12:23:31 +0900

    Improve msg handling

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,34 @@
-use lib::{github, slack};
-use serde_json::json;
 mod lib;
+use lib::{github, slack};
+use serde_json::{json, Value};
 
 #[tokio::main]
 async fn main() {
     let contribution_count = &github::todays_contribution_count().await.unwrap_or(0);
 
-    let message = if contribution_count == &0 {
-        json!({
-            "blocks": [
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "You haven't committed today."
-                    }
-                }
-            ]
-        })
-    } else {
-        // has_contribution_message()
-        json!({
-            "blocks": [
-                {
-                    "type": "section",
-                    "text": {
-                      "type": "mrkdwn",
-                      "text": you_have_commited_msg(contribution_count)
-                    }
-                }
-            ]
-        })
-    };
+    let message = create_msg_blocks(contribution_count);
     slack::send(message).await;
 }
 
-fn you_have_commited_msg(contribution_count: &i64) -> String {
-    let count_string = if contribution_count == &1 {
-        "once".to_owned()
-    } else if contribution_count == &2 {
-        "twice".to_owned()
-    } else {
-        format!("{} times", contribution_count)
-    };
-    format!("You have commited {} today.", count_string)
+fn create_msg_blocks(contribution_count: &i64) -> Value {
+    json!({
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                  "type": "mrkdwn",
+                  "text": todays_contribution_msg(contribution_count)
+                }
+            }
+        ]
+    })
+}
+
+fn todays_contribution_msg(contribution_count: &i64) -> String {
+    match contribution_count {
+        0 => "You haven't commited today.".to_owned(),
+        1 => "You have commited once today.".to_owned(),
+        2 => "You have commited twice today.".to_owned(),
+        _ => format!("You have commited {} times today.", contribution_count),
+    }
 }
