81aba7de5ff8800a5821ca28b71a802d6bba9f88
Steven <fukasesteven@gmail.com>
2022-08-05 18:11:11 +0900

    wip

--- src/usecases/get_contribution_count_usecase.rs
+++ src/usecases/get_contribution_count_usecase.rs
@@ -15,10 +15,10 @@ impl<'a> GetContributionCountUsecase<'a> {
 #[async_trait(?Send)]
 impl<'a> AbstractUsecase<i64> for GetContributionCountUsecase<'a> {
     async fn execute(&self) -> Result<i64, std::io::Error> {
-        let contribution_count = self.repository.get_contribution_count();
-        match contribution_count {
-            Ok(contribution_count) => Ok(contribution_count),
-            Err(e) => Err(e),
-        }
+        self.repository.get_contribution_count().await
+        // match contribution_count {
+        //     Ok(contribution_count) => Ok(contribution_count),
+        //     Err(e) => Err(e),
+        // }
     }
 }
