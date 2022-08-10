ade96cea69a8e94881b3b1cb10908192c0e9da70
Steven <fukasesteven@gmail.com>
2022-08-10 10:42:41 +0900

    wip message

--- src/features/daily_summary.rs
+++ src/features/daily_summary.rs
@@ -1,6 +1,9 @@
-use crate::services::github::{
-    self,
-    schemas::single_day_committed_repo::single_day_committed_repo::SingleDayCommittedRepoUserContributionsCollectionCommitContributionsByRepository as ContributionsByRepo,
+use crate::services::{
+    github::{
+        self,
+        schemas::single_day_committed_repo::single_day_committed_repo::SingleDayCommittedRepoUserContributionsCollectionCommitContributionsByRepository as ContributionsByRepo,
+    },
+    slack,
 };
 
 use serde_json::{json, Value};
@@ -11,18 +14,84 @@ pub async fn send_summary() {
     println!("{:?}", todays_contributions);
     let repo_count = todays_contributions.len();
     println!("{:?}", repo_count);
+    slack::send(message_body).await;
 }
 
 fn create_message_body(todays_contributions: &[ContributionsByRepo]) -> Value {
+    let repo_count = todays_contributions.len();
+
     json!({
         "blocks": [
             {
+                "type": "header",
+                "text": {
+                    "type": "plain_text",
+                    "text": ":computer: Activity Report",
+                    "emoji": true
+                }
+            },
+            {
                 "type": "section",
                 "text": {
-                  "type": "mrkdwn",
-                  "text": "You haven't commited today.".to_owned()
+                    "type": "mrkdwn",
+                    // "text": you_have_made_count_text(repo_count, ),
                 }
-            }
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*Commits*\n<google.com|stevenfukase/raspberrypi>\t2 commits\n<google.com|stevenfukase/actix-playground>\t2 commits"
+                }
+            },
+   
         ]
     })
 }
+
+// fn you_have_made_count_text(commit_count: i64, repo_count: i64) -> String {
+
+// }
+
+// {
+// 	"blocks": [
+// 		{
+// 			"type": "header",
+// 			"text": {
+// 				"type": "plain_text",
+// 				"text": ":computer: Activity Report",
+// 				"emoji": true
+// 			}
+// 		},
+// 		{
+// 			"type": "section",
+// 			"text": {
+// 				"type": "mrkdwn",
+// 				"text": "You have made *6* contributions on *3* repositories"
+// 			}
+// 		},
+// 		{
+// 			"type": "divider"
+// 		},
+// 		{
+// 			"type": "section",
+// 			"text": {
+// 				"type": "mrkdwn",
+// 				"text": "*Commits*\n<google.com|stevenfukase/raspberrypi>\t2 commits\n<google.com|stevenfukase/actix-playground>\t2 commits"
+// 			}
+// 		},
+// 		{
+// 			"type": "divider"
+// 		},
+// 		{
+// 			"type": "section",
+// 			"text": {
+// 				"type": "mrkdwn",
+// 				"text": "*Pull Requests*\n<google.com|stevenfukase/raspberrypi>\n<google.com|stevenfukase/actix-playground>"
+// 			}
+// 		}
+// 	]
+// }
