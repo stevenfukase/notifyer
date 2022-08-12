9c98d1a3d24b1de5a20808678cd622009728b15e
Steven <fukasesteven@gmail.com>
2022-08-13 02:00:24 +0900

    Options, Cron job

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
 pub mod features;
 pub mod services;
 use crate::features::{daily_summary::send_summary, no_commit::notify};
+use std::env;
 
 pub async fn run() {
-    notify().await;
-    send_summary().await;
+    let args = env::args().collect::<Vec<String>>();
+    // TODO: Make more elegant
+    if args.contains(&"notify".to_owned()) {
+        notify().await;
+    }
+    if args.contains(&"summary".to_owned()) {
+        send_summary().await;
+    }
 }
