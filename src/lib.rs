21460cef3419d0da277e70ad0305aaee7667b98f
Steven <fukasesteven@gmail.com>
2022-09-06 17:01:03 +0900

    added logger

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
 pub mod features;
 pub mod services;
 use crate::features::{daily_summary::send_summary, no_commit::notify};
+use log::info;
 use std::env;
 
 pub async fn run() {
     let args = env::args().collect::<Vec<String>>();
+
+    if args.is_empty() {
+        info!("args not passed.")
+    }
+
     // TODO: Make more elegant
     if args.contains(&"notify".to_owned()) {
         notify().await;
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #[tokio::main]
 async fn main() {
+    env_logger::init();
     notifyer::run().await;
 }
