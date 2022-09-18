c796907eaf94a5785e15bf9b3c009793adf3f28c
stevenfukase <fukasesteven@gmail.com>
2022-09-19 00:55:06 +0900

    simplify shell, rename

--- cross_compile.sh
+++ cross_compile.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
+#!/bin/zsh
 
-set -o errexit
-set -o nounset
-set -o pipefail
-set -o xtrace
 readonly TARGET_ARCH=armv7-unknown-linux-gnueabihf
 
 cargo build --release --target=${TARGET_ARCH}
