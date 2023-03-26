5159f4585c13325984abde661ece02f7194ff1a1
stevenfukase <fukasesteven@gmail.com>
2023-03-26 11:44:31 +0900

    cleanup

--- adapters/src/messaging/messaging_service.rs
+++ adapters/src/messaging/messaging_service.rs
@@ -2,7 +2,7 @@ use application::domains::{
     enums::application_error::ApplicationError, value_objects::message::Message,
 };
 use serde_json::json;
-use std::{thread, time};
+
 const SLACK_SEND_MESSAGE_ENDPOINT: &str = "https://slack.com/api/chat.postMessage";
 
 #[non_exhaustive]
@@ -13,34 +13,19 @@ pub struct MessagingService {
 
 impl MessagingService {
     pub async fn send(&self, message: Message) -> Result<(), ApplicationError> {
-        let delay = time::Duration::from_secs(3);
+        let request_body = json!({
+            "channel": self.slack_channel_id,
+            "blocks": message.inner()["blocks"],
+        });
 
-        let perform_request = || async {
-            let request_body = json!({
-                "channel": self.slack_channel_id,
-                "blocks": message.inner()["blocks"],
-            });
+        let result = reqwest::Client::new()
+            .post(SLACK_SEND_MESSAGE_ENDPOINT)
+            .bearer_auth(&*self.slack_bot_user_oauth_token)
+            .json(&request_body)
+            .send()
+            .await;
 
-            reqwest::Client::new()
-                .post(SLACK_SEND_MESSAGE_ENDPOINT)
-                .bearer_auth(&*self.slack_bot_user_oauth_token)
-                .json(&request_body)
-                .send()
-                .await
-        };
-
-        loop {
-            let result = perform_request().await;
-
-            log::debug!("{:?}", result);
-
-            if result.is_ok() {
-                log::info!("Executed");
-                break;
-            }
-            log::error!("Failed to send Slack. Retrying");
-            thread::sleep(delay);
-        }
+        log::debug!("{:?}", result);
 
         Ok(())
     }
